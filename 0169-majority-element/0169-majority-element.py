e8c44a8b4024213d8a5dd8c50a159f84cde47e71
Uday Guduguntla <udayguduguntla@gmail.com>
2025-05-30 19:28:48 +0530

    Time: 7 ms (57.51%), Space: 19.4 MB (19.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            d[i]=d.get(i,0)+1
+        d = sorted(d.items(), key=lambda x: x[1], reverse=True)
+        return d[0][0]
