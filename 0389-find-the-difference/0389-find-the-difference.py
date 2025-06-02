c90b1ede253a2e6efae80541676e7b67c96af46a
Uday Guduguntla <udayguduguntla@gmail.com>
2025-06-03 00:32:38 +0530

    Time: 0 ms (100%), Space: 18 MB (23.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,5 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sv=sum(map(ord,s))
-        tv=sum(map(ord,t))
-        return chr(tv-sv)
+        # sv=sum(map(ord,s))
+        # tv=sum(map(ord,t))
+        # return chr(tv-sv)
+        from functools import reduce
+        return chr(reduce(lambda a,b:a^b,map(ord,s+t)))
