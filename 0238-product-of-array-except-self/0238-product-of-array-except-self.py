095e9b8b7d1eb6fe6c0015ef74afb13163c11b81
Uday Guduguntla <udayguduguntla@gmail.com>
2025-06-02 11:52:59 +0530

    Time: 15 ms (97.09%), Space: 22.7 MB (99.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,19 @@
-import numpy as np
+from functools import reduce
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         if 0 not in nums:
-            m=np.prod(nums)
-            return [int(m//i) for i in nums]
-        else:
-            l=[1]*len(nums)
+            m=reduce(lambda x,y: x*y,nums,1)
             for i in range(len(nums)):
-                for j in range(len(nums)):
-                    if j==i:
-                        continue
-                    l[i]*=nums[j]
-            return l
+                nums[i] = m//nums[i]
+            return nums
+        else:
+            if nums.count(0) > 1:
+                return [0]*len(nums)
+            else:
+                l=[]
+                for i in range(len(nums)):
+                    if nums[i]!=0:
+                        l.append(0)
+                    else:
+                        l.append(reduce(lambda x,y : x*y,nums[:i]+nums[i+1:],1))
+                return l
