0af6958ce32afe58746d93a412de5d9f25b7139e
Uday Guduguntla <udayguduguntla@gmail.com>
2025-06-11 10:38:38 +0530

    Time: 0 ms (100%), Space: 17.8 MB (37.42%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,19 +1,26 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        from collections import Counter
-        if Counter(s1)!=Counter(s2):  return False
+        # approach - 1
+        # from collections import Counter
+        # if Counter(s1)!=Counter(s2):  return False
         if len(s1) != len(s2):  return False
-        n=len(s1)
-        i,j=0,len(s1)-1
-        c=0
-        while i<j:
-            # print(s1[i]!=s2[i], s1[i], s2[i], i, "i")
-            # print(s1[j]!=s2[j], s1[j], s2[j], j, "j")
-            if s1[i]!=s2[i]:  c+=1
-            if s1[j]!=s2[j]:  c+=1
-            if c>2:  return False
-            i+=1
-            j-=1
-        if len(s1)%2==1 and s1[n//2] != s2[n//2]:
-            c+=1
-        return True if c<=2 else False
+        # n=len(s1)
+        # i,j=0,len(s1)-1
+        # c=0
+        # while i<j:
+        #     if s1[i]!=s2[i]:  c+=1
+        #     if s1[j]!=s2[j]:  c+=1
+        #     if c>2:  return False
+        #     i+=1
+        #     j-=1
+        # if len(s1)%2==1 and s1[n//2] != s2[n//2]:
+        #     c+=1
+        # return True if c<=2 else False
+
+        # approach - 2
+        indices = [i for i in range(len(s1)) if s1[i] != s2[i]]
+        if not indices:
+            return True
+        if len(indices) != 2:
+            return False
+        return s1[indices[0]] == s2[indices[1]] and s1[indices[1]] == s2[indices[0]]
