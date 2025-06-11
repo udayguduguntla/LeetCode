d623b2601effe384f5fe2e92e1d13b3b15b4adac
Uday Guduguntla <udayguduguntla@gmail.com>
2025-06-11 13:13:53 +0530

    Time: 19 ms (17.84%), Space: 42.9 MB (9.32%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.py
+++ 2634-minimum-common-value/2634-minimum-common-value.py
@@ -1,4 +1,7 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        a = sorted(set(nums1 + nums2) - set(nums1).symmetric_difference(set(nums2)))
+        # a = sorted(set(nums1 + nums2) - set(nums1).symmetric_difference(set(nums2)))
+        # return a[0] if a else -1
+
+        a=sorted(set(nums1) & set(nums2))
         return a[0] if a else -1
