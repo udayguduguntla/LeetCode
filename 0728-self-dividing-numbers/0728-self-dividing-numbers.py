a010ccbb3f64076f7dd2d5a4d06fb7e48c20f727
Uday Guduguntla <udayguduguntla@gmail.com>
2025-06-12 17:00:03 +0530

    Time: 6 ms (72.77%), Space: 18 MB (30.52%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -6,5 +6,21 @@ class Solution:
         return sum(1 for i in digits if n%i == 0) == len(digits)
 
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        nums = [i for i in range(left,right+1) if self.checkDivisibleNumber(i)]
+        # approach - 1
+
+        # nums = [i for i in range(left,right+1) if self.checkDivisibleNumber(i)]
+        # return nums
+
+        # approach - 2
+
+        nums = []
+        for i in range(left,right+1):
+            if'0' not in str(i):
+                num = i
+                remind = 0
+                while num > 0 and remind == 0:
+                    digit = num % 10
+                    num = num // 10
+                    remind = i % digit
+                if remind == 0:  nums.append(i)
         return nums
