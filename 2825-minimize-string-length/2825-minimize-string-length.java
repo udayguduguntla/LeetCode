150c3212a38d4d38ba8319555f0dc07b645c6943
Uday Guduguntla <udayguduguntla@gmail.com>
2025-07-09 11:39:36 +0530

    Time: 8 ms (28.27%), Space: 45.1 MB (74.95%) - LeetHub

---
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int minimizedStringLength(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+        return set.size();
+    }
+}
